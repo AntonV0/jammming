183de179f07ff1d37b3dc4ead7bf0934fa508303
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-09-23 16:24:52 +0100

    Add Spotify.js for access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,29 @@
+const clientId = ""; // A specific API key will not be included in this project.
+const redirectUri = "http://localhost:3000/"
+
+let accessToken = "";
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if(accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+
+      window.setTimeout(() => accessToken = "", expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  }
+}
+
+export default Spotify

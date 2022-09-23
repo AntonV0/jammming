07f407ef3e6263cf9fb759a61cdace3e41a38b89
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-09-23 12:51:29 +0100

    Remove tracks from playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
                        { name: "Playlist Name 3", artist: "Playlist Artist 3", album: "Playlist Album 3", id: 6 }]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -26,6 +27,13 @@ class App extends React.Component {
     };
 
     tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
     this.setState({playlistTracks: tracks});
   }
 
@@ -39,7 +47,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults} 
                            onAdd={this.addTrack}/>
             <Playlist playlistName={this.state.playlistName} 
-                      playlistTracks={this.state.playlistTracks} />
+                      playlistTracks={this.state.playlistTracks}
+                      onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -10,7 +10,9 @@ class TrackList extends React.Component {
           this.props.tracks && this.props.tracks.map(track => {
             return (<Track track={track} 
                            key={track.id} 
-                           onAdd={this.props.onAdd}/>)
+                           onAdd={this.props.onAdd}
+                           onRemove={this.props.onRemove}
+                           isRemoval={this.props.isRemoval} />)
           })
         }
       </div>
